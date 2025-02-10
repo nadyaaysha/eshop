eca89a3699af517be02b32f1fcc7c99995627cf9
nadyaaysha <nadyaaysha07@gmail.com>
2025-02-10 15:15:54 +0700

    Add ProductService.java, ProductServiceImpl.java

---
+++ src/main/java/id/ac/ui/cs/advpro/eshop/service/ProductService.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advpro.eshop.service;
+
+import id.ac.ui.cs.advpro.eshop.model.Product;
+import java.util.List;
+
+public interface ProductService {
+    public Product create(Product product);
+    public List<Product> findAll();
+}
