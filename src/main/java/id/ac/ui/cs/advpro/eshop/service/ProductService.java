585f6d75bedb0b779fb960418ea6230f35063486
nadyaaysha <nadyaaysha07@gmail.com>
2025-02-18 09:19:40 +0700

    Fix: Remove unnecessary public modifiers in ProductService

--- src/main/java/id/ac/ui/cs/advpro/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advpro/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advpro.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product findById(String productId);
-    public Product updateProduct(Product updatedProduct);
-    public Product deleteProduct(String productId);
+    Product create(Product product);
+    List<Product> findAll();
+    Product findById(String productId);
+    Product updateProduct(Product updatedProduct);
+    Product deleteProduct(String productId);
 }
